625a76fe36e4d7d0042bb1f519bf1277f8cdc3c2
Gustavo Ferreira <gustavoferreira73@live.com>
2022-06-14 22:59:00 -0300

    grafico adicionado

--- funcao/app.js
+++ funcao/app.js
@@ -11,3 +11,35 @@ function chamum(){
 function fechaum(){
   imgs.style.display='none'
 }
+
+/*==========*/
+
+const labels = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'C# .NET',
+  'Firebase',
+  'SQLserve',
+];
+
+const data = {
+  labels: labels,
+  datasets: [{
+    label: 'Meus conhecimentos',
+    backgroundColor: '#1f2633',
+    borderColor: 'rgb(255, 99, 132)',
+    data: [100, 85, 45, 75, 20, 30,],
+  }]
+};
+
+const config = {
+  type: 'bar',
+  data: data,
+  options: {}
+};
+
+const myChart = new Chart(
+  document.getElementById('myChart'),
+  config
+);
