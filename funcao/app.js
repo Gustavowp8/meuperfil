510f85d901075f6b24126eec823a8a57fda81294
Gustavo Ferreira <gustavoferreira73@live.com>
2022-06-16 21:35:02 -0300

    atualização

--- funcao/app.js
+++ funcao/app.js
@@ -12,6 +12,57 @@ function fechaum(){
   imgs.style.display='none'
 }
 
+/*====== links ======*/
+
+function face(){
+  alert('indisponível')
+}
+function insta(){
+  const irinsta = confirm('Você deseja ir para o  Instagram ? ')
+  if(irinsta == true){
+    location='https://www.instagram.com/gage_developer'
+  }
+}
+
+function linkedin(){
+  const linkedin = confirm('Você deseja ir para o LinkedIn')
+  if(linkedin == true){
+  location='https://www.linkedin.com/in/gustavo-ferreira-dos-santos-103317154/'
+  }
+}
+function whast(){
+  const whastsa = confirm('Você deseja ir para WhatsApp ?')
+  if(whastsa == true){
+  location='https://api.whatsapp.com/send?phone=5561995118369&text=Ol%C3%A1%2C%20Gustavo'
+  }
+}
+function you(){
+  const yout = confirm('Você deseja ir para o YouTube ?')
+  if(yout == true){
+    location='https://www.youtube.com/channel/UC06k8u6-wycS0-6WAOdVl2Q'
+  }
+}
+function telegram(){
+  const teleg = confirm('Você deseja ir para o Telegram ?')
+  
+  if(teleg == true){
+    location='https://t.me/GustavoFerreira73'
+  }
+}
+
+function chamamsgg(){
+  msgg.style.display='block'
+}
+
+function fecharmsgtt(){
+  msgg.style.display='none'
+}
+
+const enviar = document.getElementById('enviar');
+enviar.addEventListener('click', enviarMasg)
+function enviarMasg(){
+  alert('OPS! houve um erro.')
+}
 /*==========*/
 
 const labels = [
